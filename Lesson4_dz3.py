5b28b1a98f36e5226d2aa469628eab1cca3f9740
Kurban <kurbanovkk96@mail.ru>
2022-10-11 06:38:26 +0300

    les4dz3

---
+++ Lesson4_dz3.py
@@ -0,0 +1,32 @@
+"""3 Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной
+последовательности.
+
+*Пример*
+
+- при [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]     ->        [2, 4, 5, 9]"""
+
+import random
+size_list = int(input('Введите размер массива: '))
+
+
+def fill(size_list):
+    lst = []
+    for i in range(size_list):
+        lst.append(random.randint(0, 10))
+    return lst
+
+
+new_list = fill(size_list)
+print(f'последовательность чисел до изменений {new_list}')
+
+
+list_ = []
+for i in new_list:
+    count = 0
+    for j in new_list:
+        if i == j:
+            count += 1
+    if count == 1:
+        list_.append(i)
+
+print(f'новый уникальный список: {list_}')
