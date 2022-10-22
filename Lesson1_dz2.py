e0c7b7d383d677eef36c40677442122e17fe43d7
Kurban <kurbanovkk96@mail.ru>
2022-10-22 10:40:16 +0300

    ddd

--- Lesson1_dz2.py
+++ Lesson1_dz2.py
@@ -6,28 +6,8 @@ x = int(input('Введите чило  x: '))
 y = int(input('Введите чило  y: '))
 z = int(input('Введите чило  z: '))
 
-if x == 1 and y == 1 and z == 1:
-    i = False
-
-if x == 0 and y == 0 and z == 0:
-    i = True
-
-if x == 1 and y == 1 and z == 0:
-    i = False
-
-if x == 1 and y == 0 and z == 0:
-    i = False
-
-if x == 1 and y == 0 and z == 1:
-    i = False
-
-if x == 0 and y == 0 and z == 1:
-    i == False
-
-if x == 0 and y == 1 and z == 1:
-    i == False
-
-if x == 0 and y == 1 and z == 0:
-    i == False
-
-print(i)
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            res = (not (x and y and z)) == (not x or not y or not z)
+            print(res)
---
+++ chernovik.py
@@ -0,0 +1,13 @@
+# Программа вычисления арифметического выражения заданного строкой 4 основные операции
+
+str1 = input('Введите арифметическое выражение: ')
+list1 = []
+k = -1
+
+
+for j in len(str1):
+    for i, char in enumerate(str1):
+        if char == '+' or char == '-' or char == '*' or char == '/':
+
+
+print(list1)
